acb3d5915d4d9b1d996624326cb1b91e1efe76ca
ThaiBinh <binhnguyen.cnit@gmail.com>
2026-05-18 20:58:57 +0700

    feat: auto focus input after add todo

--- src/TodoInput.jsx
+++ src/TodoInput.jsx
@@ -1,12 +1,23 @@
+import { useRef } from "react";
+
 function TodoInput({ value, setValue, onAdd }) {
+  const inputRef = useRef(null);
   return (
     <div>
       <input
+        ref={inputRef}
         type="text"
         value={value}
         onChange={(e) => setValue(e.target.value)}
       />
-      <button onClick={onAdd}>Thêm</button>
+      <button
+        onClick={() => {
+          onAdd();
+          inputRef.current.focus();
+        }}
+      >
+        Thêm
+      </button>
     </div>
   );
 }
